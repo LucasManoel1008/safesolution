0fc6ce0bd4e3a07999fa2b4c73209c5f7d824d56
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-09-04 11:42:49 -0300

    img

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from './assets/components/Footer'
 
 
 
+
 function App() {
   return (
     <div>
---
+++ src/shared/Imagesservicos.jsx
@@ -0,0 +1,7 @@
+import backgroundServicos from '../assets/img/imagensServicos/background.png'
+
+const imagensServicos = {
+    backgroundServicos,
+}
+
+export default imagensServicos
