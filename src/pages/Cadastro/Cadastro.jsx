76831959ae839361a4bf8c0798c123eba40640de
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-09-08 11:09:25 -0300

    cadastro - parte 1

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -2,33 +2,29 @@ import React from 'react'
 import '../../assets/css/cadastro.css'
 import Imagenspadroes from '../../shared/Imagespadroes'
 function Cadastro() {
+    
   return (
     <div className='cadastroContent'>
-        <section className="cadastroBox">
-            
-            <div className="form-content cadastroForm">
-                        <h4 className='mb-5'>Informações Pessoais</h4>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="Nome completo*" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="email" className="form-control" placeholder="Email de contato*"/>
-                                </div>
-                         </div>
-                        <div className="col-md-6">
-                                <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="CPF*" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="tel" className="form-control" placeholder="Telefone"/>
-                                </div>
-                         </div>
-                    </div>
-                <a href="/Cadastro2" role='button' className="continuar btn btn-primary mt-4">Continuar</a>
-            </div>
-        </section>
+       <img src={Imagenspadroes.logo} alt="Logo" width={200} className='logoCadastro' />
+       <h4>Cadastro</h4>
+       <div class="nameInput mt-4">
+            <input type="text"  className="form-control" placeholder='Primeiro nome'/>
+            <input type="text"  className="form-control" placeholder='Último nome'/>
+        </div>
+        <div className="email">
+            <input type="email" className='form-control mt-4' placeholder='Email'/>
+        </div>
+        <div className="date mt-4">
+            <label htmlFor="date">Data de nascimento</label>
+            <input type="date" className='form-control' id="start" name="trip-start" />
+        </div>
+        <div className="passwordInputCadastro mt-4 d-flex">
+            <input type="password" className='form-control' placeholder='Digite uma senha' />
+            <input type="password" className='form-control' placeholder='Confirme sua senha' />
+        </div>
+        <a href="/Cadastro2" role='button' className="continuarCadastro1 btn btn-primary mt-4">Continuar</a>
+        <span>Ja possui uma conta? <a href="/Login">Entre já</a></span>
+        <p className='termos-e-politicas mt-4'>Ao continuar, afirmo que concordo com a <a href="/Politica">Política de privacidade</a> e os <a href="/Termos">Termos de uso</a> da Safe Solutions.</p>
     </div>
   )
 }
