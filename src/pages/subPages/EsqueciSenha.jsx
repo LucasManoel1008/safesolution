e76e46300de9af6d9bda6c6db8a2e475ce32d0f3
ViniciusDeSantanaAlves <ndemedeirosrosa@gmail.com>
2024-09-12 07:43:17 -0300

    update na tela de recuperação de senha

--- src/pages/subPages/EsqueciSenha.jsx
+++ src/pages/subPages/EsqueciSenha.jsx
@@ -4,13 +4,16 @@ import Imagenspadroes from '../../shared/Imagespadroes'
 import { Link } from 'react-router-dom'
 function EsqueciSenha() {
   return (
-   <div className='contentSenha'>
+   <div className='contentSenha pt-2'>
   
-    <h4 className='aga4 pt-3'>Resgatar Senha</h4>
-    <p className='texttoilet pb-2'>Insira endereço de email valido para recuperação de senha</p>
+    <h4 className='agacuatro pt-5'>Resgatar Senha</h4>
+    <p className='texttoilet pb-2'>Insira o endereço de email registrado na sua conta <br/> que 
+    te enviaremos um código de verificação.</p>
+    
+    
 
-<input type='text' className='form-control skibidi pt-1' placeholder='E-mail' />
-<input type='text' className='form-control bolsonaro pt-1' placeholder='ta ok' />
+<input type='text' className='form-control skibidi pt-1 pb-1 p-4' placeholder='E-mail' />
+<button className='botãoIceBLue btn-primary btn'> Confirmar Email</button>
    </div>
    
   )
