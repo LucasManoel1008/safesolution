d6f9ba9f4721ec3613644491ce4ea7fa31da5b11
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-09-17 21:25:43 -0300

    mudança de provedor para contratante

--- src/pages/Cadastro/Cadastro2.jsx
+++ src/pages/Cadastro/Cadastro2.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Cadastro2() {
 
     // Autocompletar CNPJ
@@ -31,9 +32,25 @@ function Cadastro2() {
     const [bairro, setBairro] = useState('');
     const [cidade, setCidade] = useState('');
     const [numero, setNumero] = useState('');
-
+    const [select, setSelect] = useState('');
+    const navigator = useNavigate();
    
 
+    const handleSelectChangeSelect = (e) => {
+        setSelect(e.target.value);
+    }
+
+    const validarFormulario = (event) => {
+        event.preventDefault();
+
+        if (select == 'provedor'){
+            navigator('/Cadastro-Servico')
+        }
+        else{
+            navigator('/UserPage')
+        }
+
+    }
 
     const handleCepChange = (e) => {
         setCep(e.target.value);
@@ -73,91 +90,91 @@ function Cadastro2() {
 
         <h4 className="mt-4">Quase lá</h4>
         
-        <form action="">
-        <p>
-        Preencha os campos abaixo com as informações essenciais sobre o seu negócio. Essas informações nos ajudarão a conectar você com clientes e outros serviços.</p>
-            <div className="nameInput nome-cnpj mt-4 ">
-                <input type="text"  className="form-control" placeholder='Nome empresarial'/>
-                <input type="text" id="cnpj" placeholder="00.000.000/0000-00" maxLength="18" className='form-control' onChange={handleInputChange} value={cnpj} ></input>
-            </div>
-            <div className="form-floating mt-4 interesse">
-                <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                    <option defaultValue={'contratante'}>Sou Contratante</option>
-                    <option value="provedor">Sou Provedor</option>
-                </select>
-                <label htmlFor="floatingSelect">Área de Interesse</label>
-            </div>
-            <div className="form-floating descricao mt-4">
-                <textarea className="form-control " placeholder="Descrição" id="floatingTextarea2"></textarea>
-                <label htmlFor="floatingTextarea2">Descrição</label>
-            </div>
-            <div className="cepInputs mt-4">
-                <div className="itens1 mb-4">
+        <form onSubmit={validarFormulario}>
+            <p>
+            Preencha os campos abaixo com as informações essenciais sobre o seu negócio. Essas informações nos ajudarão a conectar você com clientes e outros serviços.</p>
+                <div className="nameInput nome-cnpj mt-4 ">
+                    <input type="text"  className="form-control" placeholder='Nome empresarial'/>
+                    <input type="text" id="cnpj" placeholder="00.000.000/0000-00" maxLength="18" className='form-control' onChange={handleInputChange} value={cnpj} ></input>
+                </div>
+                <div className="form-floating mt-4 interesse">
+                    <select onChange={handleSelectChangeSelect} value={select} className="form-select" id="floatingSelect" aria-label="Floating label select example">
+                        <option defaultValue={'contratante'}>Sou Contratante</option>
+                        <option value="provedor">Sou Provedor</option>
+                    </select>
+                    <label htmlFor="floatingSelect">Área de Interesse</label>
+                </div>
+                <div className="form-floating descricao mt-4">
+                    <textarea className="form-control " placeholder="Descrição" id="floatingTextarea2"></textarea>
+                    <label htmlFor="floatingTextarea2">Descrição</label>
+                </div>
+                <div className="cepInputs mt-4">
+                    <div className="itens1 mb-4">
+                    <div>
+                        <input
+                            type="text"
+                            id="cep"
+                            value={cep}
+                            onChange={handleCepChange}
+                            className="form-control"
+                            placeholder="CEP"
+                            
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="rua"
+                            value={rua}
+                            readOnly
+                            maxLength={5}
+                            className="form-control"
+                            placeholder="Rua"
+                        />
+                    </div>
+                    </div>
+                
+                <div className="itens2 mb-4">
+                
+                <div>
+
+                    <input
+                        type="text"
+                        id="bairro"
+                        value={bairro}
+                        readOnly
+                        className="form-control"
+                        placeholder="Bairro"
+                    />
+                </div>
                 <div>
                     <input
                         type="text"
-                        id="cep"
-                        value={cep}
-                        onChange={handleCepChange}
+                        id="numero"
+                        value={numero}
+                        onChange={handleNumeroChange}
                         className="form-control"
-                        placeholder="CEP"
+                        placeholder="Número"
                         
                     />
                 </div>
-                <div>
+                </div>
+                
+                <div className="mb-4">
                     <input
                         type="text"
-                        id="rua"
-                        value={rua}
+                        id="cidade"
+                        value={cidade}
                         readOnly
-                        maxLength={5}
                         className="form-control"
-                        placeholder="Rua"
+                        placeholder="Cidade"
                     />
                 </div>
+                <div className="">
+                    <button type="button" className="btn btn-primary aplicar" onClick={getCEP}>Aplicar</button>
                 </div>
-            
-            <div className="itens2 mb-4">
-            
-            <div>
-
-                <input
-                    type="text"
-                    id="bairro"
-                    value={bairro}
-                    readOnly
-                    className="form-control"
-                    placeholder="Bairro"
-                />
-            </div>
-            <div>
-                <input
-                    type="text"
-                    id="numero"
-                    value={numero}
-                    onChange={handleNumeroChange}
-                    className="form-control"
-                    placeholder="Número"
-                    
-                />
-            </div>
-            </div>
-            
-            <div className="mb-4">
-                <input
-                    type="text"
-                    id="cidade"
-                    value={cidade}
-                    readOnly
-                    className="form-control"
-                    placeholder="Cidade"
-                />
-            </div>
-            <div className="">
-                <button type="button" className="btn btn-primary aplicar" onClick={getCEP}>Aplicar</button>
-            </div>
-            </div>
-            <Link to="/UserPage" role='button' className="continuarCadastro1 btn btn-primary mt-4">Finalizar</Link>
+                </div>
+                <button role='submit' className="continuarCadastro1 btn btn-primary mt-4">Finalizar</button>
         </form>
        
             
