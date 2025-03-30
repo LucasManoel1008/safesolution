71003badd65d7c5ac561105cf2689bd12110c0bf
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2025-03-30 20:40:00 -0300

    Implementa a página de configurações do usuário com opções de alteração de senha e autenticação de dois fatores, além de adicionar estilos personalizados.

--- src/pages/UserPage/UserSettings.jsx
+++ src/pages/UserPage/UserSettings.jsx
@@ -1,12 +1,43 @@
 import React from "react";
-
+import stylesPadroes from '../../assets/css/userProfile.module.css';
+import styles from '../../assets/css/userSettings.module.css';
 const UserSettings = () => (
-    <div className='userSettings mt-4'>
-     <h2 className='text-center textocentral'> Segurança e Privacidade </h2>
-     <p className='text-center bru'>Gerencie as configuranças de segurança de sua conta, altere a senha da conta e ative autenticação de dois fatores.</p>
-      <h5 className='text-center'>Esta seção ainda não está pronta</h5>
-      <h5 className='text-center'>Volte novamente mais tarde</h5>
-      <strong className='iconClosed'><i className="fa-solid fa-square-xmark"></i></strong>
-    </div>
+        <>
+          <div className={`${stylesPadroes.userProfile} p-4 mb-4`}>
+              <div className={stylesPadroes.userPageHeader}>
+                <h4 className="text-center">Configurações de conta</h4>
+                <p>Gerencie as configurações de segurança de sua conta</p>
+              </div>
+            </div>
+            <div className={`${stylesPadroes.mainContent} p-4 mb-4`}>
+              <div className={`${styles.alterarSenha} d-flex flex-column`}>
+                  <h5 className="mb-2 blue2">Alterar Senha</h5>
+                  <p>Para a sua segurança, um código será enviado ao email cadastrado para finalizar a alteração de senha.</p>
+                  <button className={`btn btn-outline-secondary mt-2 `}>Alterar Senha</button>
+              </div>
+              <div className={`${styles.alterarSenha} mt-4`}>   
+                  <h5 className="mb-2 blue2">Autenticação de Dois Fatores</h5>
+                  <p>A Autenticação de Dois Fatores (ADF) adiciona uma camada de proteção a mais na sua conta. <a href="https://www.microsoft.com/pt-br/security/business/security-101/what-is-two-factor-authentication-2fa" target="_blank">Saiba mais</a></p>
+                  
+                  <p className="mt-2">Desativadas</p>
+                  <div className={`${styles.opcaoAutenticacao} mt-2 d-flex flex-row`}>
+                    <i className="fa-solid fa-envelope"></i>
+                    <div>
+                      <p>Email</p>
+                      <p>Utilize o email para validar a entrada na conta</p>
+                    </div>
+                  </div>  
+                  <div className={`${styles.opcaoAutenticacao} mt-2 d-flex flex-row`}>
+                    <i className="fa-solid fa-shield"></i>
+                    <div>
+                      <p>Aplicativo Autenticador</p>
+                      <p>Utilize um aplicativo autenticador para validar a entrada na conta</p>
+                    </div>
+                  </div>  
+                  
+
+              </div>
+            </div>
+          </>
   );
   export default UserSettings;
