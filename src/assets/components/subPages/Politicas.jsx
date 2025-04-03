47524b719ffaf0a9875c6144a1f2820eca0851a1
marcos.moura <marcos.moura@novigisolution.com.br>
2025-04-03 18:37:15 -0300

    Politicas

--- src/assets/components/subPages/Politicas.jsx
+++ src/assets/components/subPages/Politicas.jsx
@@ -3,118 +3,87 @@ import '../../css/politica.css';
 
 function Politicas() {
   return (
-    <div className='conteudoPoliticas'>
+    <div className="containerPoliticas">
       <h1>Política de Privacidade</h1>
-      
-      <section>
-        <h2>1. Introdução</h2>
-        <p>
-          A Safe Solutions valoriza a sua privacidade e está comprometida em proteger suas informações pessoais. 
-          Esta Política de Privacidade descreve como coletamos, usamos, divulgamos e protegemos as informações de nossos usuários e parceiros.
-        </p>
-      </section>
+      <div className="conteudoPoliticas">
+        <section>
+          <h2>1. Introdução</h2>
+          <p>
+            A Safe Solutions valoriza a sua privacidade e está comprometida em proteger suas informações pessoais. 
+            Esta Política de Privacidade descreve como coletamos, usamos, divulgamos e protegemos as informações de nossos usuários e parceiros.
+          </p>
+        </section>
 
-      <section>
-        <h2>2. Informações Que Coletamos</h2>
-        <ul>
-          <li><strong>Informações de Contato:</strong> Nome da empresa, endereço, e-mail, número de telefone e outras informações de contato.</li>
-          <li><strong>Informações de Negócios:</strong> Dados sobre suas operações comerciais, serviços oferecidos, contratos e transações.</li>
-          <li><strong>Informações de Uso:</strong> Dados sobre como você interage com nossos serviços, incluindo registros de acesso, dados de navegação e preferências.</li>
-          <li><strong>Informações de Pagamento:</strong> Informações necessárias para processar pagamentos, como dados bancários e cartões.</li>
-        </ul>
-      </section>
+        <section>
+          <h2>2. Informações Que Coletamos</h2>
+          <ul>
+            <li><strong>Informações de Contato:</strong> Nome da empresa, endereço, e-mail, número de telefone e outras informações de contato.</li>
+            <li><strong>Informações de Negócios:</strong> Dados sobre suas operações comerciais, serviços oferecidos, contratos e transações.</li>
+            <li><strong>Informações de Uso:</strong> Dados sobre como você interage com nossos serviços, incluindo registros de acesso, dados de navegação e preferências.</li>
+            <li><strong>Informações de Pagamento:</strong> Informações necessárias para processar pagamentos, como dados bancários e cartões.</li>
+          </ul>
+        </section>
 
-      <section>
-        <h2>3. Como Usamos Suas Informações</h2>
-        <ul>
-          <li><strong>Prestação de Serviços:</strong> Para fornecer e gerenciar nossos serviços e soluções, incluindo a manutenção de sua conta e a execução de contratos.</li>
-          <li><strong>Comunicação:</strong> Para entrar em contato com você sobre atualizações, ofertas e informações importantes relacionadas aos nossos serviços.</li>
-          <li><strong>Análise e Melhoria:</strong> Para analisar o uso dos nossos serviços e melhorar nossa plataforma e processos.</li>
-          <li><strong>Segurança:</strong> Para proteger a segurança e integridade de nossos serviços e prevenir fraudes ou abusos.</li>
-        </ul>
-      </section>
+        <section>
+          <h2>3. Como Usamos Suas Informações</h2>
+          <ul>
+            <li><strong>Prestação de Serviços:</strong> Para fornecer e gerenciar nossos serviços e soluções, incluindo a manutenção de sua conta e a execução de contratos.</li>
+            <li><strong>Comunicação:</strong> Para entrar em contato com você sobre atualizações, ofertas e informações importantes relacionadas aos nossos serviços.</li>
+            <li><strong>Análise e Melhoria:</strong> Para analisar o uso dos nossos serviços e melhorar nossa plataforma e processos.</li>
+            <li><strong>Segurança:</strong> Para proteger a segurança e integridade de nossos serviços e prevenir fraudes ou abusos.</li>
+          </ul>
+        </section>
 
-      <section>
-        <h2>4. Compartilhamento de Informações</h2>
-        <ul>
-          <li><strong>Com Parceiros e Prestadores de Serviços:</strong> Podemos compartilhar suas informações com parceiros e prestadores de serviços que atuam em nosso nome, como processadores de pagamento e empresas de análise.</li>
-          <li><strong>Com Autoridades Legais:</strong> Podemos divulgar informações quando exigido por lei, regulamentação ou para proteger nossos direitos e interesses legais.</li>
-          <li><strong>Transferências de Negócios:</strong> Em caso de fusão, aquisição ou venda de ativos, suas informações podem ser transferidas como parte do negócio.</li>
-        </ul>
-      </section>
+        <section>
+          <h2>4. Compartilhamento de Informações</h2>
+          <ul>
+            <li><strong>Com Parceiros e Prestadores de Serviços:</strong> Podemos compartilhar suas informações com parceiros e prestadores de serviços que atuam em nosso nome, como processadores de pagamento e empresas de análise.</li>
+            <li><strong>Com Autoridades Legais:</strong> Podemos divulgar informações quando exigido por lei, regulamentação ou para proteger nossos direitos e interesses legais.</li>
+            <li><strong>Transferências de Negócios:</strong> Em caso de fusão, aquisição ou venda de ativos, suas informações podem ser transferidas como parte do negócio.</li>
+          </ul>
+        </section>
 
-      <section>
-        <h2>5. Segurança das Informações</h2>
-        <p>
-          Implementamos medidas de segurança apropriadas para proteger suas informações contra acesso não autorizado, alteração, divulgação ou destruição. 
-          No entanto, nenhuma medida de segurança é totalmente infalível, e não podemos garantir a segurança absoluta de seus dados.
-        </p>
-      </section>
+        <section>
+          <h2>5. Segurança das Informações</h2>
+          <p>
+            Implementamos medidas de segurança apropriadas para proteger suas informações contra acesso não autorizado, alteração, divulgação ou destruição. 
+            No entanto, nenhuma medida de segurança é totalmente infalível, e não podemos garantir a segurança absoluta de seus dados.
+          </p>
+        </section>
 
-      <section>
-        <h2>6. Seus Direitos</h2>
-        <p>
-          Você tem o direito de acessar, corrigir ou excluir suas informações pessoais.
-        </p>
-      </section>
+        <section>
+          <h2>6. Seus Direitos</h2>
+          <p>
+            Você tem o direito de acessar, corrigir ou excluir suas informações pessoais.
+          </p>
+        </section>
 
-      <section>
-        <h2>7. Cookies e Tecnologias Similares</h2>
-        <p>
-          Utilizamos cookies e tecnologias semelhantes para melhorar sua experiência em nossa plataforma e analisar o uso. 
-          Você pode configurar seu navegador para recusar cookies, mas isso pode afetar sua capacidade de usar nossos serviços de forma eficaz.
-        </p>
-      </section>
+        <section>
+          <h2>7. Cookies e Tecnologias Similares</h2>
+          <p>
+            Utilizamos cookies e tecnologias semelhantes para melhorar sua experiência em nossa plataforma e analisar o uso. 
+            Você pode configurar seu navegador para recusar cookies, mas isso pode afetar sua capacidade de usar nossos serviços de forma eficaz.
+          </p>
+        </section>
 
-      <section>
-        <h2>8. Alterações na Política de Privacidade</h2>
-        <p>
-          Podemos atualizar esta Política de Privacidade ocasionalmente para refletir mudanças em nossos serviços ou requisitos legais. 
-          Notificaremos sobre qualquer alteração substancial por meio de nossos canais de comunicação habituais.
-        </p>
-      </section>
+        <section>
+          <h2>8. Alterações na Política de Privacidade</h2>
+          <p>
+            Podemos atualizar esta Política de Privacidade ocasionalmente para refletir mudanças em nossos serviços ou requisitos legais. 
+            Notificaremos sobre qualquer alteração substancial por meio de nossos canais de comunicação habituais.
+          </p>
+        </section>
 
-      <section>
-        <h2>9. Políticas Relacionadas a Orçamentos e Proteção de Riscos</h2>
-        <p>
-          A Safe Solutions adota políticas claras e transparentes em relação à elaboração e aceitação de orçamentos, com o objetivo de proteger tanto o contratante quanto o provedor de serviços de riscos financeiros e operacionais.
-        </p>
-        <h3>9.1 Transparência no Orçamento</h3>
-        <ul>
-          <li><strong>Clareza na Proposta:</strong> Todos os orçamentos fornecidos pela Safe Solutions serão detalhados de forma clara, especificando os serviços incluídos, prazos e condições de pagamento. Isso garante que o contratante compreenda completamente o escopo e as responsabilidades envolvidas.</li>
-          <li><strong>Validade do Orçamento:</strong> Os orçamentos têm um período de validade especificado, dentro do qual as condições acordadas serão mantidas. Após esse período, pode ser necessário um novo orçamento em função de mudanças nas condições de mercado ou na natureza dos serviços solicitados.</li>
-        </ul>
-
-        <h3>9.2 Proteção ao Contratante</h3>
-        <ul>
-          <li><strong>Garantia de Preço Fixo:</strong> Após a aceitação do orçamento, a Safe Solutions garante que os valores acordados não sofrerão alterações, salvo se houver mudanças no escopo dos serviços ou necessidades adicionais solicitadas pelo contratante.</li>
-          <li><strong>Compromisso de Entrega:</strong> A Safe Solutions se compromete a entregar os serviços dentro dos prazos acordados, com a qualidade esperada e em conformidade com o que foi descrito no orçamento.</li>
-          <li><strong>Mitigação de Riscos:</strong> Se houver qualquer risco operacional que possa impactar o cumprimento do orçamento ou dos prazos, a Safe Solutions informará imediatamente o contratante e buscará uma solução alternativa com o menor impacto possível.</li>
-        </ul>
-
-        <h3>9.3 Proteção ao Provedor de Serviços</h3>
-        <ul>
-          <li><strong>Revisão de Escopo:</strong> Caso o contratante solicite modificações significativas após a aceitação do orçamento inicial, a Safe Solutions se reserva o direito de revisar o orçamento, ajustando os custos e prazos de acordo com as novas exigências.</li>
-          <li><strong>Compensação por Cancelamento:</strong> No caso de cancelamento por parte do contratante após a aceitação do orçamento e início dos serviços, a Safe Solutions poderá cobrar uma taxa de cancelamento proporcional ao trabalho já realizado, para cobrir custos e esforços investidos até aquele momento.</li>
-          <li><strong>Exclusão de Responsabilidade por Força Maior:</strong> A Safe Solutions não será responsável por atrasos ou falhas na entrega dos serviços devido a eventos de força maior, como desastres naturais, interrupções tecnológicas significativas ou mudanças legais que impactem a execução dos serviços.</li>
-        </ul>
-
-        <h3>9.4 Revisão e Ajustes</h3>
-        <ul>
-          <li><strong>Revisão de Orçamentos:</strong> Ambas as partes podem solicitar revisões dos orçamentos em caso de mudanças significativas no escopo dos serviços ou em condições externas que influenciem os custos.</li>
-          <li><strong>Compromisso de Negociação:</strong> Em caso de disputa ou desacordo sobre o orçamento ou a execução dos serviços, as partes se comprometem a buscar uma solução amigável por meio de negociação direta, antes de recorrer a medidas legais.</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>10. Contato</h2>
-        <p>
-          Se você tiver dúvidas ou preocupações sobre esta Política de Privacidade, entre em contato conosco em: 
-          <a href="mailto:safesolutionsempresa@gmail.com">safesolutionsempresa@gmail.com</a>
-        </p>
-      </section>
+        <section>
+          <h2>10. Contato</h2>
+          <p>
+            Se você tiver dúvidas ou preocupações sobre esta Política de Privacidade, entre em contato conosco em: 
+            <a href="mailto:safesolutionsempresa@gmail.com">safesolutionsempresa@gmail.com</a>
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
 
-export default Politicas;
+export default Politicas;
