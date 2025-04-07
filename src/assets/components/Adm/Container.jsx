b4ae82580962fba4a94eec49a660c7d0dc83560f
ndemedeirosrosa@gmail.com <ndemedeirosrosa@gmail.com>
2025-04-07 10:55:23 -0300

    base da pagina adm

---
+++ src/assets/components/Adm/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../../css/admpage.module.css'
+function Container() {
+  return (
+    <div className={`${style.Admcontainer}`}>
+        Container
+    
+    
+    </div>
+  )
+}
+
+export default Container
---
+++ src/assets/components/Adm/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../../css/admpage.module.css'
+
+
+function Head() {
+  return (
+    <div className={`${style.admHeader}`}>
+        Cabeçalho
+    </div>
+  )
+}
+
+export default Head
