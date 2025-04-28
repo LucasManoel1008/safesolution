56b422605aba274454573b412f7b21b826b1722a
RaphaelBoas64 <emailgit>
2025-04-28 08:10:11 -0300

    manutenção

--- src/assets/components/Servicos/ServicosFiltter.jsx
+++ src/assets/components/Servicos/ServicosFiltter.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "../../../pages/Servicos/servicos.module.css";
+import axios from "axios";
 
 function ServicosFiltter({ onFilterChange }) {
   const [categoria, setCategoria] = useState("");
@@ -13,72 +14,91 @@ function ServicosFiltter({ onFilterChange }) {
     onFilterChange(filtros);
   };
 
+  const enviarFiltros = (e) => {
+    e.preventDefault();
+    const FiltroServicos={
+        categoria, data, precoMax, area
+    }
+    axios.post(
+
+    )
+  }
+
   return (
-    <div className={`${styles.servicosFiltter}`}>
-      <div className={`${styles.filtroHeader}`}>
-        <h5>Filtros</h5>
-      </div>
-      <hr />
 
-      <div className="categoriaFiltro mb-4">
-        <span>Categoria</span>
-        <select className="form-select w-75" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
-          <option value="">Todas</option>
-          <option value="Conserto">Conserto</option>
-          <option value="Jardinagem">Limpeza</option>
-          <option value="Segurança">Segurança</option>
-          <option value="Encanador">Encanador</option>
-          <option value="Eletrônicos">Eletrônicos</option>
-        </select>
-      </div>
-      <hr />
 
-      <div className="dataFiltro mb-4">
-        <span>Data</span>
-        <select className="form-select w-75" value={data} onChange={(e) => setData(e.target.value)}>
-          <option value="">Todas</option>
-          <option value="recentes">Mais recentes</option>
-          <option value="antigos">Mais antigos</option>
-        </select>
-      </div>
-      <hr />
+      <form onSubmit={(e) => enviarFiltros(e)} >
 
-      <div className="precoFiltro mb-4">
-        <span className="d-block">Preço máximo: R$ {precoMax}</span>
-        <input className="w-75" type="range" min="0" max="9999" step="10" value={precoMax} onChange={(e) => setPrecoMax(Number(e.target.value))} />
-      </div>
-      <hr />
+          <div className={`${styles.servicosFiltter}`}>
+              <div className={`${styles.filtroHeader}`}>
+                  <h5>Filtros</h5>
+              </div>
+              <hr/>
 
-      <div className="localizacaoFiltro mb-4">
-        <span className="d-block">Área de atuação do serviço</span>
-        <select className="form-select w-75" value={area} onChange={(e) => setArea(e.target.value)}>
-          <option value="">Todas</option>
-          <option value="Barueri">Barueri</option>
-          <option value="Osasco">Osasco</option>
-          <option value="Carapicuiba">Carapicuiba</option>
-          <option value="Itapevi">Itapevi</option>
-          <option value="Barra funda">Barra funda</option>
-        </select>
-      </div>
 
-      <div className="botoesAcao">
-        <button className="btn btn-primary d-block col-12 mb-2" onClick={aplicarFiltros}>
-          Aplicar
-        </button>
-        <button
-          className="btn btn-outline-secondary col-12"
-          onClick={() => {
-            setCategoria("");
-            setData("");
-            setPrecoMax(0);
-            setArea("");
-            onFilterChange({ categoria: "", data: "", precoMax: 9999, area: "" });
-          }}
-        >
-          Limpar
-        </button>
-      </div>
-    </div>
+              <div className="categoriaFiltro mb-4">
+                  <span>Categoria</span>
+                  <select className="form-select w-75" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
+                      <option value="">Todas</option>
+                      <option value="Conserto">Conserto</option>
+                      <option value="Jardinagem">Limpeza</option>
+                      <option value="Segurança">Segurança</option>
+                      <option value="Encanador">Encanador</option>
+                      <option value="Eletrônicos">Eletrônicos</option>
+                  </select>
+              </div>
+              <hr/>
+
+              <div className="dataFiltro mb-4">
+                  <span>Data</span>
+                  <select className="form-select w-75" value={data} onChange={(e) => setData(e.target.value)}>
+                      <option value="">Todas</option>
+                      <option value="recentes">Mais recentes</option>
+                      <option value="antigos">Mais antigos</option>
+                  </select>
+              </div>
+              <hr/>
+
+              <div className="precoFiltro mb-4">
+                  <span className="d-block">Preço máximo: R$ {precoMax > 0 ? precoMax : "-"}</span>
+                  <input className="w-75" type="range" min="0" max="9999" step="10" value={precoMax}
+                         onChange={(e) => setPrecoMax(Number(e.target.value))}/>
+              </div>
+              <hr/>
+
+              <div className="localizacaoFiltro mb-4">
+                  <span className="d-block">Área de atuação do serviço</span>
+                  <select className="form-select w-75" value={area} onChange={(e) => setArea(e.target.value)}>
+                      <option value="">Todas</option>
+                      <option value="Barueri">Barueri</option>
+                      <option value="Osasco">Osasco</option>
+                      <option value="Carapicuiba">Carapicuiba</option>
+                      <option value="Itapevi">Itapevi</option>
+                      <option value="Barra funda">Barra funda</option>
+                  </select>
+              </div>
+
+              <div className="botoesAcao">
+                  <button className="btn btn-primary d-block col-12 mb-2" onClick={aplicarFiltros}>
+                      Aplicar
+                  </button>
+                  <button
+                      className="btn btn-outline-secondary col-12"
+                      onClick={() => {
+                          setCategoria("");
+                          setData("");
+                          setPrecoMax(0);
+                          setArea("");
+                          onFilterChange({categoria: "", data: "", precoMax: 9999, area: ""});
+                      }}
+                  >
+
+                      Limpar
+                  </button>
+
+              </div>
+          </div>
+      </form>
   );
 }
 
--- src/assets/js/Filtros.js
+++ src/assets/js/Filtros.js
@@ -11,7 +11,7 @@ class Filtros {
       return (
         (this.categoria === "" || servico.categoria.toLowerCase() === this.categoria.toLowerCase()) &&
         (this.area === "" || servico.area.toLowerCase() === this.area.toLowerCase()) &&
-        (servico.preco <= this.precoMax) &&
+        (this.precoMax > 0 ? servico.preco <= this.precoMax  : servico.preco) &&
         (this.data === "" ||
           (this.data === "recentes" && servico.data >= "2025-04-01") ||
           (this.data === "antigos" && servico.data < "2025-04-01"))
