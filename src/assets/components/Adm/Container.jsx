6a545db25cbcf6e279298f6ba8ea50a5b3e78a36
brunomagno16 <brunomagno842@gmail.com>
2025-05-14 11:46:15 -0300

    ADM att

--- src/assets/components/Adm/Container.jsx
+++ src/assets/components/Adm/Container.jsx
@@ -3,13 +3,24 @@ import style from '../../css/admpage.module.css';
 import axios from 'axios';
  
 function Container() {
-  const [usuarios, setUsuarios] = useState([]);
+  const [empresa, setEmpresa] = useState([]);
+  const [isLoading, setLoading] = useState(true)
   
   
-  const recuperarUsuario = () => {
-      axios.get('http://localhost:5173/usuario/listar').then((response) =>{
+  const recuperarUsuario = async () => {
+    try{
+      await axios.get('http://localhost:8080/empresa')
+      .then((response) =>{
         console.log(response.data)
+        setEmpresa(response.data)
+        setLoading(false)
+      }).catch((error) => {
+        console.log(error)
       })
+    }
+    catch{
+      console.log("Erro no banco")
+    }
     
   }
   useEffect(() =>{
@@ -18,7 +29,7 @@ function Container() {
   
  
   const handlePromover = (id) => {
-    setUsuarios(prev =>
+    setEmpresa(prev =>
       prev.map(user =>
         user.id === id ? { ...user, tipo: 'admin' } : user
       )
@@ -26,7 +37,7 @@ function Container() {
   };
  
   const handleToggleStatus = (id) => {
-    setUsuarios(prev =>
+    setEmpresa(prev =>
       prev.map(user =>
         user.id === id
           ? {
@@ -39,48 +50,53 @@ function Container() {
   };
  
   const handleExcluir = (id) => {
-    setUsuarios(prev => prev.filter(user => user.id !== id));
+    setEmpresa(prev => prev.filter(user => user.id !== id));
   };
  
   return (
     <div className={style.Admcontainer}>
       <h2>Usuários</h2>
+      
       <table style={{ width: '100%', borderCollapse: 'collapse' }}>
         <thead>
           <tr style={{ textAlign: 'left', borderBottom: '2px solid #ccc' }}>
-            <th style={{ padding: '12px 16px' }}>ID</th>
-            <th style={{ padding: '12px 16px' }}>Nome</th>
+            <th style={{ padding: '12px 16px' }}>CPF</th>
+            <th style={{ padding: '12px 16px' }}>Nome empresa</th>
             <th style={{ padding: '12px 16px' }}>Email</th>
-            <th style={{ padding: '12px 16px' }}>Tipo</th>
-            <th style={{ padding: '12px 16px' }}>Status</th>
+            <th style={{ padding: '12px 16px' }}>Dono</th>
             <th style={{ padding: '12px 16px' }}>Ações</th>
           </tr>
         </thead>
+        {isLoading ? 
+        <tr>
+        <td colSpan="6" className="text-center">
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Carregando...</span>
+            </div>
+        </td>
+      </tr>
+        :
         <tbody>
-          {usuarios.map((usuario) => (
-            <tr key={usuario.id} style={{ borderBottom: '1px solid #eee' }}>
-              <td style={{ padding: '10px 16px' }}>{usuario.id}</td>
-              <td style={{ padding: '10px 16px' }}>{usuario.nome}</td>
-              <td style={{ padding: '10px 16px' }}>{usuario.email}</td>
-              <td style={{ padding: '10px 16px' }}>{usuario.tipo}</td>
-              <td style={{ padding: '10px 16px' }}>
-                {usuario.status === 'online' ? (
-                  <span style={{ color: 'green' }}>🟢 online</span>
-                ) : (
-                  <span style={{ color: 'gray' }}>⚪ offline</span>
-                )}
-              </td>
-              <td style={{ padding: '10px 16px' }}> 
-                <button className='btn btn-secondary' onClick={() => handleToggleStatus(usuario.id)} style={{ marginRight: 5 }}>
-                  {usuario.status === 'online' ? 'Bloquear' : 'Desbloquear'}
-                </button>
-                <button className='btn btn-danger' onClick={() => handleExcluir(usuario.id)}>
-                  Excluir
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        {empresa.map((empresa) => (
+          <tr key={empresa.cnpj} style={{ borderBottom: '1px solid #eee' }}>
+            <td style={{ padding: '10px 16px' }}>{empresa.cnpj}</td>
+            <td style={{ padding: '10px 16px' }}>{empresa.nome_empresa}</td>
+            <td style={{ padding: '10px 16px' }}>{empresa.usuario.email}</td>
+            <td style={{ padding: '10px 16px' }}>{empresa.usuario.nome_usuario}</td>
+            
+            <td style={{ padding: '10px 16px' }}> 
+              <button className='btn btn-secondary' onClick={() => handleToggleStatus(empresa.cnpj)} style={{ marginRight: 5 }}>
+                {empresa.status === 'online' ? 'Bloquear' : 'Desbloquear'}
+              </button>
+              <button className='btn btn-danger' onClick={() => handleExcluir(empresa.cnpj)}>
+                Excluir
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    }
+        
  
       </table>
     </div>
