482e691776dc5188d011b1181d41208052372f60
brunomagno16 <brunomagno842@gmail.com>
2025-05-19 11:22:08 -0300

    Adm link att

--- src/assets/components/Adm/Container.jsx
+++ src/assets/components/Adm/Container.jsx
@@ -23,6 +23,30 @@ function Container() {
     }
     
   }
+  function getDataTable(id) {
+    $(id).DataTable({
+        "ordering": true,
+        "paging": false,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            }
+        }
+    });
+}
   useEffect(() =>{
     recuperarUsuario()
   }, [])
@@ -96,6 +120,9 @@ function Container() {
         ))}
       </tbody>
     }
+    
+
+
         
  
       </table>
