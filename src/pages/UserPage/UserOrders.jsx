af7baa178513240b237ebc38135be7da80741e96
Lkzin0122 <bitencourtes@gmail.com>
2025-05-22 08:32:32 -0300

    Alteração tela Compras

--- src/pages/UserPage/UserOrders.jsx
+++ src/pages/UserPage/UserOrders.jsx
@@ -3,26 +3,28 @@ import Styles from '../../assets/css/UserOrder.module.css';
 
 const Card = ({ title, description, status }) => {
   return (
-    <div className="card">
+    <div className={Styles.card}>
       <h3 className="card-title">{title}</h3>
       <p className="card-description">{description}</p>
-      <p className="card-status">{status}</p>
+      <p className="card-status">Data: {status}</p>
+      <button className={Styles.detailsButton}>Detalhes</button>    
     </div>
   );
 };
 
 const UserOrder = () => {
   const orders = [
-    { id: 1, title: "Consultoria Empresarial", description: "Description for Order 1", status: "Em andamento" },
-    { id: 2, title: "Transposte de Cargas", description: "Description for Order 2", status: "Em andamento" },
-    { id: 3, title: "Gestão de Redes Socias", description: "Description for Order 3",  status: "Concluido" },
+    { id: 1, title: "Consultoria Empresarial", description: "Description for Order 1", status: "12/04/2024" },
+    { id: 2, title: "Transposte de Cargas", description: "Description for Order 2", status: "15/07/2024" },
+    { id: 3, title: "Gestão de Redes Socias", description: "Description for Order 3",  status: "20/06/2024" },
     // Add mais pedidos conforme necessário
   ];
 
   return (
-    <div className="User-order">
-      <h2 className="text-center">Compras</h2>
-      <div className={Styles.card}>
+    <div className={Styles.header}>
+      <h2 className={Styles.title}>Compras</h2>
+      <p className={Styles.text}>Gerencie as compras de sua conta</p>
+      <div className={Styles.container}>
         {orders.map((order) => (
           <Card key={order.id} title={order.title} description={order.description} status={order.status} />
         ))}
