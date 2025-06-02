7f41522defbd8ec7405770bfc7ae0e96d33ece6a
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2025-06-02 08:28:57 -0300

    Implement admin login functionality with input handling and API integration

---
+++ src/Services/AdmFunctions/AdmHandleInputs.jsx
@@ -0,0 +1,7 @@
+export const handleInputChangeUserName = (e, setUsername) => {
+    setUsername(e.target.value);
+}
+
+export const handleInputChangePassword = (e, setPassword) => {
+    setPassword(e.target.value);
+}
--- src/pages/adm/LoginAdm.jsx
+++ src/pages/adm/LoginAdm.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from '../../assets/css/LoginAdm.module.css'
+import * as Handle from '../../Services/AdmFunctions/AdmHandleInputs'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 function LoginAdm() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const realzarLogin = (e) => {
+    e.preventDefault();
+    try{
+      const response = axios.post('http://localhost:8080/adm/login', {
+        nome_adm: username,
+        senha_adm: password
+      });
+      response.then((res) => {
+        console.log(res.data);
+      });
+    }
+    catch (error) {
+      console.error('Erro ao realizar login:', error);
+    }
+  }
+
   return (
     <div className={`${Styles.loginPage}`}>
         <h4 className='blue2 bold mt-4'>Login</h4>
         <p className='opacity-75 blue2'>Acesso administrativo</p>
 
         <form className={`${Styles.formLogin}`}>
-        <div class="form-group w-75">
-            <label for="nomeUsuario">Nome de Usuário</label>
-            <input type="text" class="form-control" id="nomeUsuario" />
+        <div className="form-group w-75">
+            <label htmlFor="nomeUsuario">Nome de Usuário</label>
+            <input type="text" className="form-control" value={username} onChange={(e) => Handle.handleInputChangeUserName(e, setUsername)}  id="nomeUsuario" />
           </div>
-          <div class="form-group w-75">
-            <label for="senhaUsuario">Senha</label>
-            <input type="password" class="form-control" id="senhaUsuario" />
+          <div className="form-group w-75">
+            <label htmlFor="senhaUsuario">Senha</label>
+            <input type="password" className="form-control" id="senhaUsuario" value={password} onChange={(e) => Handle.handleInputChangePassword(e, setPassword)} />
           </div>
-            <button className={`${Styles.buttonLogin} w-75 btn btn-primary col-12`}>Entrar</button>
+            <button className={`${Styles.buttonLogin} w-75 btn btn-primary col-12`} type='button' onClick={(e) => realzarLogin(e)}>Entrar</button>
         </form>
-
-
-
-    </div>
+    </div>  
   )
 }
 
